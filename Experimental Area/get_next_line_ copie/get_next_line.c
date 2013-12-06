3a80d50f9f03d1fdfab03f51d75b7e38257c01f3
Arnaud Debray <Arno@arno-macbookpro.home>
2013-12-06 13:32:48 +0100

    norminage en cours

--- Experimental Area/get_next_line_ copie/get_next_line.c
+++ Experimental Area/get_next_line_ copie/get_next_line.c
@@ -13,35 +13,6 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-/* int 	get_next_line(int const fd, char **line)
-{
-	static char	*array;
-	char		buffer[BUFF_SIZE];
-	int 		i;
-
-	if (!array)
-	{
-		array = ft_memalloc(1);
-		while ((i = read(fd, buffer, BUFF_SIZE - 1 )) != 0)
-		{
-			array = ft_strjoin(array, buffer);
-			ft_bzero(buffer, BUFF_SIZE);
-			ft_putnbr(i);
-			ft_putendl(" ");
-		}
-	}
-	i = 0;
-	while (array[i] != '\n')
-		i = i + 1;
-	*line = ft_strsub(array, 0, i);
-	if (i == 0)
-		*line = ft_strsub("", 0 , 1);
-	array = ft_strsub(array, i + 1, ft_strlen(array));
-	if (!array)
-		return (0);
-	return (1);
-} */
-
 char	*ft_strnjoin(char const *s1, char const *s2, int n)
 {
 	char	*tmp;
