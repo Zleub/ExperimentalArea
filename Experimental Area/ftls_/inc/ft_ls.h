cd6ede2d6abd4993f804c94cdf487c28ae3ac288
Arnaud DEBRAY <adebray@student.42.fr>
2013-12-07 22:16:17 +0100

    norme gnl + ftls arch
    Former-commit-id: 0b0ed296dee5914342b62de7b0cc38f4f07c0ee6

--- Experimental Area/ftls_/inc/ft_ls.h
+++ Experimental Area/ftls_/inc/ft_ls.h
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2013/12/01 23:29:11 by adebray           #+#    #+#             */
-/*   Updated: 2013/12/07 22:10:37 by adebray          ###   ########.fr       */
+/*   Created: 2013/12/07 22:07:12 by adebray           #+#    #+#             */
+/*   Updated: 2013/12/07 22:13:42 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ft_ls.h>
+#ifndef FT_LS_H
+# define FT_LS_H
 
-int 	main(int argc, char **argv)
-{
-	int 	i;
+# include <libft.h>
 
-	i = 1;
-	ft_putnbr(argc - 1);
-	ft_putendl(" arguments.");
-	while (i <= argc - 1)
-	{
-		ft_putendl(argv[i]);
-		i = i + 1;
-//		catch_argument;
-	}
-	return (0);
-}
+#endif
