a58df2441172fa52d399b23977662e0e02358d04
Arnaud DEBRAY <adebray@student.42.fr>
2013-12-27 02:48:09 +0100

    shity up
    Former-commit-id: 0fa100e47c914c9002025a6a8d2eac57ef750985

--- Experimental Area/push_swap/inc/push_swap.h
+++ Experimental Area/push_swap/inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/23 04:36:24 by adebray           #+#    #+#             */
-/*   Updated: 2013/12/26 10:35:16 by adebray          ###   ########.fr       */
+/*   Updated: 2013/12/27 02:46:50 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,51 +31,56 @@
 # include <stdlib.h>
 # include <ft_printf.h>
 
-typedef struct			s_list
+typedef struct		s_list
 {
-	int					data;
-	struct s_list		*next;
-}						t_list;
+	int				data;
+	struct s_list	*next;
+}					t_list;
 
-typedef struct			s_this_is_glob_stuct
+typedef struct		s_this_is_glob_stuct
 {
-	int					i;
-	int					max;
-	t_list				**l_a;
-	t_list				**l_b;
-}						t_vars;
+	int				i;
+	int				max;
+	t_list			**l_a;
+	t_list			**l_b;
+}					t_vars;
 
-t_list					*ft_initshackle(void);
-t_vars					*ft_initvars(void);
-void					ft_buildlist(t_vars *vars, int argc, char **argv);
-void					ft_printargv(int argc, char **argv);
-void					ft_putendl_fd(char const *s, int fd);
-void					ft_putchar_fd(char c, int fd);
-void					ft_printshackle(t_list *shackle);
-void					ft_swap(t_list **head);
-void					ft_push(t_list **dst, t_list **src);
-void					ft_reverse(t_list **head);
-void					ft_rreverse(t_list **head);
-char					*ft_some_option(t_vars *vars, int argc, char **argv);
-void					ft_interactive(t_vars *vars, char *str);
-void					ft_isswap(t_vars *vars, char *str);
-void					ft_ispush(t_vars *vars, char *str);
-void					ft_isreverse(t_vars *vars, char *str);
-void					ft_isrreverse(t_vars *vars, char *str);
-void					ft_isprint(t_vars *vars, char *str);
-void					ft_dumplist(t_list **head);
-void					ft_print_la(t_vars *vars);
-void					ft_print_lb(t_vars *vars);
-void					ft_sa(t_vars *vars);
-void					ft_sb(t_vars *vars);
-void					ft_ss(t_vars *vars);
-void					ft_pb(t_vars *vars);
-void					ft_pa(t_vars *vars);
-void					ft_ra(t_vars *vars);
-void					ft_rb(t_vars *vars);
-void					ft_rr(t_vars *vars);
-void					ft_rra(t_vars *vars);
-void					ft_rrb(t_vars *vars);
-void					ft_rrr(t_vars *vars);
+t_list				*ft_initshackle(void);
+t_vars				*ft_initvars(void);
+void				ft_buildlist(t_vars *vars, int argc, char **argv);
+void				ft_printargv(int argc, char **argv);
+void				ft_putendl_fd(char const *s, int fd);
+void				ft_putchar_fd(char c, int fd);
+void				ft_printshackle(t_list *shackle);
+void				ft_swap(t_list **head);
+void				ft_push(t_list **dst, t_list **src);
+void				ft_reverse(t_list **head);
+void				ft_rreverse(t_list **head);
+char				*ft_some_option(t_vars *vars, int argc, char **argv);
+void				ft_interactive(t_vars *vars, char *str);
+void				ft_isswap(t_vars *vars, char *str);
+void				ft_ispush(t_vars *vars, char *str);
+void				ft_isreverse(t_vars *vars, char *str);
+void				ft_isrreverse(t_vars *vars, char *str);
+void				ft_isprint(t_vars *vars, char *str);
+void				ft_dumplist(t_list **head);
+void				ft_print_la(t_vars *vars);
+void				ft_print_lb(t_vars *vars);
+void				ft_sa(t_vars *vars);
+void				ft_sb(t_vars *vars);
+void				ft_ss(t_vars *vars);
+void				ft_pb(t_vars *vars);
+void				ft_pa(t_vars *vars);
+void				ft_ra(t_vars *vars);
+void				ft_rb(t_vars *vars);
+void				ft_rr(t_vars *vars);
+void				ft_rra(t_vars *vars);
+void				ft_rrb(t_vars *vars);
+void				ft_rrr(t_vars *vars);
+void				ft_solve(t_vars *vars);
+void				ft_reverso(t_vars *vars, t_list *l_b, t_list *tmp, int i);
+void				ft_postmax(t_vars *vars, t_list *l_a, int i);
+void				ft_bswap(t_vars *vars, t_list *l_b, t_list *tmp, int i);
+
 
 #endif
--- Experimental Area/push_swap/main.c
+++ Experimental Area/push_swap/main.c
@@ -6,89 +6,11 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/16 20:25:00 by adebray           #+#    #+#             */
-/*   Updated: 2013/12/26 10:39:17 by adebray          ###   ########.fr       */
+/*   Updated: 2013/12/27 02:46:18 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
-
-void			ft_solve(t_vars *vars)
-{
-	t_list		*l_a;
-	t_list		*l_b;
-	t_list		*tmp;
-	int			i;
-
-	l_a = *vars->l_a;
-	while (l_a->data != vars->max)
-	{
-		l_a = l_a->next;
-		ft_pb(vars);
-		ft_printf(" ");
-		l_b = *vars->l_b;
-		if (l_b->next && l_b->data < l_b->next->data)
-		{
-			if (!l_b->next->next)
-			{
-				ft_sb(vars);
-				ft_printf(" ");
-			}
-			else if (l_b->next->next && l_b->data > l_b->next->next->data)
-			{
-				ft_sb(vars);
-				ft_printf(" ");
-			}
-			else
-			{
-				i = 1;
-				tmp = *vars->l_b;
-				while (tmp->next)
-					tmp = tmp->next;
-				while (l_b->data > tmp->data)
-				{
-					i += 1;
-					if (l_b->data > tmp->data)
-					{
-						ft_rrb(vars);
-						ft_printf(" ");
-						ft_sb(vars);
-						ft_printf(" ");
-					}
-					else
-					{
-						ft_rb(vars);
-						ft_printf(" ");
-					}
-					tmp = *vars->l_b;
-					while (tmp->next)
-						tmp = tmp->next;
-				}
-				while (i != 0)
-				{
-					ft_rb(vars);
-					ft_printf(" ");
-					i -= 1;
-				}
-			}
-		}
-
-	}
-	if (l_a->next)
-	{
-		ft_ra(vars);
-		ft_printf(" ");
-		ft_solve(vars);
-	}
-	i = -1;
-	while (*vars->l_b)
-	{
-		ft_pa(vars);
-		if (*vars->l_b)
-			ft_printf(" ");
-	}
-	if (i == -1)
-		ft_printf("\n");
-}
 
 int				main(int argc, char **argv)
 {
