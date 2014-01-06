53aed392afa8200a626cdfe951ea4b78758dacad
Arnaud DEBRAY <adebray@student.42.fr>
2014-01-06 15:22:16 +0100

    save up
    Former-commit-id: 361502da62bb12e3932d4be8fa8d5003a6bf724a

--- Experimental Area/ft_select/inc/ft_select.h
+++ Experimental Area/ft_select/inc/ft_select.h
@@ -6,7 +6,7 @@
 /*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/01/02 17:27:11 by adebray           #+#    #+#             */
-/*   Updated: 2014/01/04 18:13:31 by adebray          ###   ########.fr       */
+/*   Updated: 2014/01/06 09:39:36 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,27 @@
 
 # include <stdlib.h>
 
-# include <curses.h>
+// # include <curses.h>
 # include <term.h>
 # include <termcap.h>
+
+# define KEY_UP read_char[0] == 27 && read_char[1] == 91 && read_char[2] == 65
+# define KEY_DW read_char[0] == 27 && read_char[1] == 91 && read_char[2] == 66
+# define KEY_RT read_char[0] == 27 && read_char[1] == 91 && read_char[2] == 67
+# define KEY_LF read_char[0] == 27 && read_char[1] == 91 && read_char[2] == 68
+
 
 int		ft_putschar(int c);
 
 typedef struct		s_list
 {
 	char			*str;
+	char			slc;
 	int				len;
 	int				x;
 	int				y;
 	struct s_list	*next;
+	struct s_list	*prev;
 }					t_list;
 
 typedef struct		s_term
